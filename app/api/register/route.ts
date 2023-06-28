f91e40e273e26c4db63b28e55be5f4332f7e2560
teppei-nishi <mluosviec@gmail.com>
2023-06-28 19:07:47 +0900

    no message

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
 type Credentials = {
@@ -5,7 +6,22 @@ type Credentials = {
   password: string
 }
 
+const prisma = new PrismaClient()
+
 export async function POST(request: Request) {
   const body: Credentials = await request.json()
-  return NextResponse.json({ message: `${body.email}を登録しました。` })
+
+  try {
+    const user = await prisma.user.create({
+      data: body,
+    })
+    console.log(user)
+    return NextResponse.json({ message: '登録しました。' })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { message: '登録できませんでした。' },
+      { status: 400 }
+    )
+  }
 }
