de9b39d54489731421579e48c5645a95d6b4785e
teppei-nishi <mluosviec@gmail.com>
2023-06-28 13:14:20 +0900

    no message

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 
 export default function handler(req: Request, res: Response) {
-  res.status(200).json({ message: `${req.body.email}を登録しました。` });
+  if (req.method === 'POST') {
+    res.status(200).json({ message: `${req.body.email}を登録しました。` });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
 }
