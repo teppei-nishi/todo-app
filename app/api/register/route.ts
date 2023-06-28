229c39a0897700c26384919d1dc2523b1e2d70b9
teppei-nishi <mluosviec@gmail.com>
2023-06-29 02:20:32 +0900

    no message

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
+import { z } from 'zod'
 
 type Credentials = {
   email: string
@@ -10,6 +11,27 @@ const prisma = new PrismaClient()
 
 export async function POST(request: Request) {
   const body: Credentials = await request.json()
+  const CredentialsSchema = z.object({
+    email: z
+      .string()
+      .email({ message: 'メールアドレスの形式が正しくありません。' }),
+    password: z
+      .string()
+      .regex(/^[a-zA-Z0-9]+$/, {
+        message: 'パスワードは半角英数字で入力してください。',
+      })
+      .min(8, { message: 'パスワードは8文字以上です。' })
+      .max(32, { message: 'パスワードは32文字以下です。' }),
+  })
+
+  const result = CredentialsSchema.safeParse(body)
+
+  if (!result.success) {
+    return NextResponse.json(
+      { message: result.error.issues.map((issue) => issue.message).join('\n') },
+      { status: 400 }
+    )
+  }
 
   try {
     const user = await prisma.user.create({
